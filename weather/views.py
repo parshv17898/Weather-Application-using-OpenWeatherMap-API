74daee10d53f823ee4b1b787d26e55ca6307c464
parshv17898 <41836462+parshv17898@users.noreply.github.com>
2020-06-16 23:25:24 +0530

    Add files via upload

---
+++ weather/views.py
@@ -0,0 +1,53 @@
+import requests
+from django.shortcuts import render
+from .models import City
+from .forms import CityForm
+
+def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=86cba73e751c57c77cbca39a4d18eefb'
+
+    err_msg = ''
+
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+
+        if form.is_valid():
+            new_city = form.cleaned_data['name']
+            existing_city_count = City.objects.filter(name=new_city).count()
+
+            if existing_city_count == 0:
+                r = requests.get(url.format(new_city)).json()
+                print(r)
+
+                if r['cod'] == 200:
+                    form.save()
+                else:
+                    err_msg = 'City does not exist in the world!'
+            else:
+                err_msg = 'City already exists in the database!'
+
+    print(err_msg)
+
+    form = CityForm()
+
+    cities = City.objects.all()
+
+    weather_data = []
+     
+    for city in cities:
+
+        r = requests.get(url.format(city)).json()
+
+        city_weather = {
+            'city' : city.name,
+            'temperature' : r['main']['temp'],
+            'description' : r['weather'][0]['description'],
+            'icon' : r['weather'][0]['icon'],
+            'pressure' : r['main']['pressure'],
+            'wind' : r['wind']['speed'],
+        }
+
+        weather_data.append(city_weather)
+
+    context = {'weather_data' : weather_data, 'form' : form}
+    return render(request, 'weather/weather.html', context)
